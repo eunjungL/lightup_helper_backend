7484faea7a61593ad6fbf44ceb7fe466719d8a09
eunjungL <dldmswjd322@gmail.com>
2021-08-11 02:16:40 +0900

    User related View add

--- lightup/models.py
+++ lightup/models.py
@@ -6,15 +6,17 @@ from location_field.models.plain import PlainLocationField
 # User
 class UserInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    lend_state = models.BooleanField(default=False)
+    borrow_state = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
 
 
-class UserBorrowState(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-    lend = models.BooleanField(default=False)
-    borrow = models.BooleanField(default=False)
+# class UserBorrowState(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+#     lend = models.BooleanField(default=False)
+#     borrow = models.BooleanField(default=False)
 
 
 class UserLocation(models.Model):
--- lightup_restAPI/views.py
+++ lightup_restAPI/views.py
@@ -3,9 +3,14 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import permissions
-from lightup.models import UserInfo
-from chat.models import Message
-from lightup_restAPI.serializers import UserInfoSerializer, LoginSerializer, MessageSerializer
+from lightup_restAPI.serializers import *
+from rest_framework import generics, response
+
+
+# User
+class LoginView(TokenObtainPairView):
+    permission_classes = [permissions.AllowAny, ]
+    serializer_class = LoginSerializer
 
 
 class UserInfoViewSet(ModelViewSet):
@@ -13,11 +18,40 @@ class UserInfoViewSet(ModelViewSet):
     serializer_class = UserInfoSerializer
 
 
-class LoginView(TokenObtainPairView):
-    permission_classes = [permissions.AllowAny, ]
-    serializer_class = LoginSerializer
+class UserBorrowStateUpdateView(generics.UpdateAPIView):
+    queryset = UserInfo.objects.all()
+    serializer_class = UserInfoSerializer
+
+    def partial_update(self, request, *args, **kwargs):
+        queryset = self.queryset.get(user=self.request.user)
+        serializer = self.serializer_class(queryset, data=request.data, partial=True)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return response.Response(serializer.data)
 
 
+class UserLocationViewSet(ModelViewSet):
+    queryset = UserLocation.objects.all()
+    serializer_class = UserLocationSerializer
+
+
+class UserLocationUpdateView(generics.UpdateAPIView):
+    queryset = UserLocation.objects.all()
+    serializer_class = UserLocationSerializer
+
+    def partial_update(self, request, *args, **kwargs):
+        queryset = self.queryset.get(user=self.request.user)
+        serializer = self.serializer_class(queryset, data=request.data, partial=True)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return response.Response(serializer.data)
+
+
+# Chat
 class ChatViewSet(ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
