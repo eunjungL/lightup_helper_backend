13a8e848414abc91106faa4e33e443d076584a59
eunjungL <dldmswjd322@gmail.com>
2021-08-19 21:22:41 +0900

    get chatting

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,7 +4,7 @@ from asgiref.sync import sync_to_async
 from chat.models import Message
 from lightup.models import UserInfo
 from channels.db import database_sync_to_async
-from send_notification import send_noti
+from pyfcm import FCMNotification
 from django.http import request, HttpResponse
 
 
@@ -65,3 +65,19 @@ class ChatConsumer(AsyncWebsocketConsumer):
     @database_sync_to_async
     def get_name(self, user):
         return user.user.username
+
+
+APIKey = "AAAA5_P06TA:APA91bHWs9LZLi2_yu4Pqxr2RyzEbAToDyyvpoxm_eeEzUTTBr7wyDMwpAQUvrLQesTg6bU-T7fN263ww4mPrDD7Ya1OuEDQKcyLG4ETdJt5ZaN_oHrUSHVT3NGw2iaERn8EYyDhs2b6"
+
+push_service = FCMNotification(APIKey)
+
+
+def send_noti(receive_user, send_user, context):
+    data_message = {
+        "contents": context,
+        "title": "%s에게서 새로운 메시지가 왔습니다." % send_user
+    }
+
+    result = push_service.notify_topic_subscribers(topic_name=receive_user, data_message=data_message)
+
+    print(result)
